11a9fad34e11953276e2de2cdd2a091e3abd2db8
Kenan BAŞDEMİR <kenan.basdemir5858@gmail.com>
2021-01-31 19:23:40 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using Client.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ChatEndpoint.ChatEndpointClient(channel);
+            var reply = client.SendText(
+                              new TextRequest { TextData = "Send someone text!" });
+            Console.WriteLine("Server: " + reply.Result);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Services/ChatService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using Server.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services
+{
+    public class ChatService : ChatEndpoint.ChatEndpointBase
+    {
+        public override Task<TextResponse> SendText(TextRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new TextResponse
+            {
+                Result = "Request handled: " + request.TextData
+            });
+        }
+    }
+}
